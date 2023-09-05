e1758a49dd731a76e58c396faf5679029d9474be
Thiago Kachinsky <thiagokachidev@gmail.com>
2023-09-04 21:26:43 -0300

    feat: create customized hooks and change components style to mobile

--- front/src/components/ContactForm/index.js
+++ front/src/components/ContactForm/index.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import isEmailValid from '../../utils/isEmailValid';
+import formatPhone from '../../utils/formatPhone';
+import { useErrors } from '../../hooks/useErrors';
 
 import { Form, ButtonContainer } from './styles';
 
@@ -12,22 +14,24 @@ import Select from '../Select';
 import Button from '../Button';
 
 export function ContactForm({ buttonLabel }) {
+  const {
+    setError, removeError, getErrorMessageByField, errors,
+  } = useErrors();
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
-  const [errors, setErrors] = useState([]);
+
+  const isFormValid = (name && errors.length === 0);
 
   function handleNameChange(event) {
     setName(event.target.value);
 
     if (!event.target.value) {
-      setErrors((prevState) => [
-        ...prevState,
-        { field: 'name', message: 'Nome é obrigatório' },
-      ]);
+      setError({ field: 'name', message: 'Nome é obrigatório' });
     } else {
-      setErrors((prevState) => prevState.filter((error) => error.field !== 'name'));
+      removeError('name');
     }
   }
 
@@ -35,25 +39,14 @@ export function ContactForm({ buttonLabel }) {
     setEmail(event.target.value);
 
     if (event.target.value && !isEmailValid(event.target.value)) {
-      const errorAlreadyExists = errors.find((error) => error.field === 'email');
-
-      if (errorAlreadyExists) {
-        return;
-      }
-
-      setErrors((prevState) => [
-        ...prevState,
-        { field: 'email', message: 'E-mail inválido' },
-      ]);
+      setError({ field: 'email', message: 'E-mail inválido' });
     } else {
-      setErrors((prevState) => prevState.filter((error) => error.field !== 'email'));
+      removeError('email');
     }
   }
 
-  function getErrorMessageByField(fieldName) {
-    const error = errors.find((errorItem) => errorItem.field === fieldName);
-
-    return error ? error.message : '';
+  function handlePhoneChange(event) {
+    setPhone(formatPhone(event.target.value));
   }
 
   function handleSubmit(event) {
@@ -62,7 +55,7 @@ export function ContactForm({ buttonLabel }) {
     const data = {
       name,
       email,
-      phone,
+      phone: phone.replace(/\D/g, ''),
       category,
     };
 
@@ -70,10 +63,10 @@ export function ContactForm({ buttonLabel }) {
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit} noValidate>
       <FormGroup error={getErrorMessageByField('name')}>
         <Input
-          placeholder="Nome"
+          placeholder="Nome *"
           value={name}
           onChange={handleNameChange}
         />
@@ -81,6 +74,7 @@ export function ContactForm({ buttonLabel }) {
 
       <FormGroup error={getErrorMessageByField('email')}>
         <Input
+          type="email"
           placeholder="E-mail"
           value={email}
           onChange={handleEmailChange}
@@ -91,15 +85,13 @@ export function ContactForm({ buttonLabel }) {
         <Input
           placeholder="Telefone"
           value={phone}
-          onChange={(event) => {
-            setPhone(event.target.value);
-          }}
+          onChange={handlePhoneChange}
+          maxLength={15}
         />
       </FormGroup>
 
       <FormGroup>
         <Select
-          placeholder="Telefone"
           value={category}
           onChange={(event) => {
             setCategory(event.target.value);
@@ -112,7 +104,7 @@ export function ContactForm({ buttonLabel }) {
       </FormGroup>
 
       <ButtonContainer>
-        <Button type="submit">
+        <Button type="submit" disabled={!isFormValid}>
           {buttonLabel}
         </Button>
       </ButtonContainer>
