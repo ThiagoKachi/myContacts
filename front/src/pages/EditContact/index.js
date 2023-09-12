792829a4a24106624e5144581cb7b403b296ff85
Thiago Kachinsky <thiagokachidev@gmail.com>
2023-09-11 22:25:11 -0300

    feat: get contact data in edit page

--- front/src/pages/EditContact/index.js
+++ front/src/pages/EditContact/index.js
@@ -1,12 +1,52 @@
+/* eslint-disable react/jsx-no-bind */
+import { useEffect, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+
 import { ContactForm } from '../../components/ContactForm';
 import { PageHeader } from '../../components/PageHeader';
+import { Loader } from '../../components/Loader';
+
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
 
 export default function EditContact() {
+  const { id } = useParams();
+  const history = useHistory();
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+
+        console.log(contactData);
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {
+    //
+  }
+
   return (
     <>
+      <Loader isLoading={isLoading} />
       <PageHeader title="Editar Thiago Kachinsky" />
 
-      <ContactForm buttonLabel="Salvar alterações" />
+      <ContactForm
+        buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
